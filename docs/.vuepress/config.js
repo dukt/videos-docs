1535fd74533eeb5125b21cdbc16c433ea3b8e27c
Benjamin David <ben@dukt.net>
2021-03-04 14:52:14 +0100

    Updated to `vuepress` 1.8

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,14 @@ module.exports = {
     title: 'Videos Documentation',
     description: 'Videos Documentation',
     base: '/docs/videos/v2/',
-    ga: 'UA-1547168-20',
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-1547168-20'
+            }
+        ]
+    ],
     themeConfig: {
         docsRepo: 'dukt/videos-docs',
         docsDir: 'docs',
