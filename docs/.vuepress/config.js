0e81cc9546d1bfdc765bbaa3f5cfadccf1541a11
Benjamin David <ben@dukt.net>
2021-03-17 12:56:37 +0100

    Removed nav links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,13 +16,6 @@ module.exports = {
         docsBranch: 'v2',
         editLinks: true,
         editLinkText: 'Edit this page on GitHub',
-        nav: [
-            { text: 'Analytics', link: 'https://dukt.net/docs/analytics/v4/' },
-            { text: 'Facebook', link: 'https://dukt.net/docs/facebook/v2/'},
-            { text: 'Social', link: 'https://dukt.net/docs/social/v2/' },
-            { text: 'Twitter', link: 'https://dukt.net/docs/twitter/v2/'},
-            { text: 'Videos', link: '/' },
-        ],
         sidebar: {
             '/': [
                 {
